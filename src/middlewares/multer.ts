49939782f0269622f7d55cc9bd58e6a6e86e7eba
Shubham Prakash <shubhamprakash681@gmail.com>
2024-07-21 03:03:18 +0530

    multer middleware bug fix

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/temp/my-uploads");
+    cb(null, "./public/temp/my-uploads");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
