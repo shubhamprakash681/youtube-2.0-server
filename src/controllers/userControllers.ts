3b29d16dfacde969ce60fcc829b627e6fdfd29ed
Shubham Prakash <shubhamprakash681@gmail.com>
2024-09-26 23:05:22 +0530

    Full registerUser controller added

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -4,6 +4,8 @@ import { StatusCodes } from "http-status-codes";
 import { registerUserSchemaValidator } from "../schema/registerUserSchema";
 import ErrorHandler from "../utils/ErrorHandler";
 import UserModel from "../models/userModel";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import APIResponse from "../utils/APIResponse";
 
 interface IRegisterUserBody {
   username: string;
@@ -65,21 +67,74 @@ export const registerUser = asyncHandler(
 
     // check for images, check for avatar
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-    const myFirstAvatarFile = files["avatar"]?.at(0);
-    // console.log("files: ", files);
-    console.log("myFirstAvatarFile: ", myFirstAvatarFile);
+    const avatar = files["avatar"]?.at(0);
+    const coverImage = files["coverImage"]?.at(0);
+    let coverImageRes = null;
 
-    // upload images, avatar to cloudinary
+    if (!avatar) {
+      return next(
+        new ErrorHandler("Avatar is required!", StatusCodes.BAD_REQUEST)
+      );
+    }
+
+    // upload images- avatar, coverImage to cloudinary
+    const avatarCloudRes = await uploadOnCloudinary(
+      avatar.path,
+      `user/${username}/avatar`
+    );
+    if (coverImage) {
+      const coverImageCloudRes = await uploadOnCloudinary(
+        coverImage.path,
+        `user/${username}/coverImage`
+      );
+
+      if (coverImageCloudRes) {
+        coverImageRes = coverImageCloudRes;
+      }
+    }
+
     // check for avatar uploaded successfully
+    if (!avatarCloudRes) {
+      return next(
+        new ErrorHandler(
+          "User Registration Failed! Please try after some time.",
+          StatusCodes.INTERNAL_SERVER_ERROR
+        )
+      );
+    }
 
     // create user object - create entry in db
-
-    // remove password and refresh token field
-    // check for user creation - return res
-
-    res.status(StatusCodes.CREATED).json({
-      success: true,
-      files,
+    const user = await UserModel.create({
+      username,
+      email,
+      fullname,
+      password,
+      avatar: avatarCloudRes.url,
+      coverImage: coverImageRes ? coverImageRes.url : "",
     });
+
+    // full proof method -- ensuring by making an extra DB call
+    // remove password and refresh token field
+    const userData = await UserModel.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    // check for user creation - return res
+    if (!userData) {
+      return next(
+        new ErrorHandler(
+          "User Registration Failed! Please try after some time.",
+          StatusCodes.INTERNAL_SERVER_ERROR
+        )
+      );
+    }
+
+    return res
+      .status(StatusCodes.CREATED)
+      .json(
+        new APIResponse(StatusCodes.CREATED, "User Registered Successfully!", {
+          user: userData,
+        })
+      );
   }
 );
