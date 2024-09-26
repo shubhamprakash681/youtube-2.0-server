df58fc637e1b54703a65ec9c224a88d2e6da3f4e
Shubham Prakash <shubhamprakash681@gmail.com>
2024-09-26 22:25:30 +0530

    Cloudinary file upload utility enhancement

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -2,31 +2,37 @@ import { UploadApiResponse } from "cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 export const uploadOnCloudinary = async (
-  localFilePath: string
+  localFilePath: string,
+  cloudinaryFolderName?: string
 ): Promise<UploadApiResponse | null> => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
   try {
     if (!localFilePath) return null;
 
     // upload file on cloudinary
     const res = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
+      folder: cloudinaryFolderName
+        ? `youtube-2.0/${cloudinaryFolderName}`
+        : "youtube-2.0",
     });
 
     // if file uploaded successfully
-    console.log("File is uploaded on cloudnary, res: ", res);
+    console.log("File is uploaded on cloudnary");
 
     return res;
   } catch (err: any) {
     // remove the locally saved temporary file
-    fs.unlinkSync(localFilePath);
+    console.warn("Cloudinary file upload failed, error: ", err);
 
     return null;
+  } finally {
+    fs.unlinkSync(localFilePath);
   }
 };
